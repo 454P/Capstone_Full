d647faa9e0ae96d0a70127a75ab7b976da1e3be2
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2023-11-15 21:35:55 +0900

    feat: recoil 상태 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 import StyleProvider from '@/styles/StyleProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <StyleProvider>
+  <StyleProvider>
+    <RecoilRoot>
       <App />
-    </StyleProvider>
-  </React.StrictMode>,
+    </RecoilRoot>
+  </StyleProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
