9f670ad9e0370845fcacc4eeab5cd21df95afffd
KZRT <kzrt0123@cau.ac.kr>
2023-11-15 18:56:09 +0900

    fix: query updated using ` instead of string identifier

--- showyourhands/src/services/signup.service.js
+++ showyourhands/src/services/signup.service.js
@@ -1,7 +1,7 @@
 const connection = require('../config/db.config');
 async function responseSignup(id, password, nickname, email){
     const query = `
-        INSERT INTO "capstone.user" (user_id, user_password, user_name, user_email)
+        INSERT INTO capstone."user" (user_id, user_password, user_name, user_email)
         VALUES ($1, $2, $3, $4);
     `;
     let result = null;
