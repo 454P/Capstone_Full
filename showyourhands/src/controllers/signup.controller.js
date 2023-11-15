a799f8b953027e1ec39350bed0c0d6444ec228b5
KZRT <kzrt0123@cau.ac.kr>
2023-11-15 18:44:05 +0900

    fix: error on signup changed to matching property

--- showyourhands/src/controllers/signup.controller.js
+++ showyourhands/src/controllers/signup.controller.js
@@ -2,9 +2,10 @@ const signupService = require('../services/signup.service')
 
 async function signupRequest(req, res, next) {
     try {
+        console.log(req.body);
         res.json(await signupService.responseSignup(req.body.id, req.body.password, req.body.nickname, req.body.email));
     } catch (err) {
-        console.error('Error on login', err.message);
+        console.error('Error on signup', err.message);
         next(err)
     }
 }
