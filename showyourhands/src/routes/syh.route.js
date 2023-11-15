32c522924f61a73b0bd7f965eadadac79d1cc42b
KZRT <kzrt0123@cau.ac.kr>
2023-11-15 17:09:30 +0900

    feat: route updated

--- showyourhands/src/routes/syh.route.js
+++ showyourhands/src/routes/syh.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/login.controller')
 
-router.get('/login', loginController.loginRequest);
+router.post('/login', loginController.loginRequest);
 
 module.exports = router;
