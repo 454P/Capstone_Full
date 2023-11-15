1a9b5a909af1f3ed9f1120239768d08bf866e536
KZRT <kzrt0123@cau.ac.kr>
2023-11-15 17:35:37 +0900

    feat: add swagger

---
+++ swagger/service/QueryService.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * login attempt
+ * Adds an item to the system
+ *
+ * no response value expected for this operation
+ **/
+exports.loginPOST = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
