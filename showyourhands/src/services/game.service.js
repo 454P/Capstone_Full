e1c3fe1c5206ab290394bd584a98a0e1adf35e07
KZRT <kzrt0123@cau.ac.kr>
2023-11-16 19:11:47 +0900

    fix: game count increased

--- showyourhands/src/services/game.service.js
+++ showyourhands/src/services/game.service.js
@@ -24,7 +24,7 @@ async function responseGameStart(api){
 }
 
 async function responseGameNext(api, count) {
-    if (count >= 2) {
+    if (count >= 5) {
         return {
             status: 401,
             message: "Game End",
