67703c4c52f35294217548681c8e9aec4b83be47
KZRT <kzrt0123@cau.ac.kr>
2023-11-16 17:32:17 +0900

    feat: add webClient with proper word

--- registerTcpServer/server.go
+++ registerTcpServer/server.go
@@ -13,6 +13,12 @@ type User struct {
 	Api  string `json:"api"`
 }
 
+type WebClient struct {
+	Type int    `json:"type"`
+	Api  string `json:"api"`
+	Word string `json:"word"`
+}
+
 func connection(conn net.Conn, channel chan []byte) {
 	// try to read
 	for {
@@ -48,7 +54,7 @@ func connection(conn net.Conn, channel chan []byte) {
 	}
 }
 
-func modelServer(conn net.Conn, channel chan []byte) {
+func modelServer(conn net.Conn, channel chan []byte), wordChannel chan string)
 	// if channel has data, then send to client
 	for {
 		data := <-channel
@@ -96,6 +102,70 @@ func modelServer(conn net.Conn, channel chan []byte) {
 		}
 		packet := dataBuff
 		fmt.Println("Read from model server: ", string(packet))
+		wordChannel <- string(packet)
+	}
+}
+
+func webClientConnection(conn net.Conn, existingConn net.Conn, wordChannel chan string){
+	for {
+		var dataBuff []byte
+		var readComplete bool
+		for {
+			tmpBuff := make([]byte, 1024)
+			n, err := conn.Read(tmpBuff)
+			if err != nil {
+				fmt.Println("conn.Read() returned from model", err.Error())
+				if err == io.EOF {
+					fmt.Println("Client closed connection")
+					conn.Close()
+					return
+				} else {
+					continue
+				}
+			}
+
+			fmt.Println("Read", n, "bytes: ", string(tmpBuff))
+			// if packet is "0000000000", then break
+			if strings.HasSuffix(strings.TrimSpace(strings.Trim(string(tmpBuff), "\x00")), "0000000000") {
+				readComplete = true
+				tmpBuff = []byte(strings.TrimSuffix(strings.TrimSpace(strings.Trim(string(tmpBuff), "\x00")), "0000000000"))
+				n = len(tmpBuff)
+			}
+			dataBuff = append(dataBuff, tmpBuff[:n]...)
+			if readComplete {
+				break
+			}
+		}
+
+		clientJson := WebClient{}
+		err := json.Unmarshal(dataBuff, &clientJson)
+		if err != nil {
+			fmt.Println("Fail to unmarshal json: ", err)
+			return
+		}
+		fmt.Println("Read from web client: ", clientJson.Word)
+		_, err = existingConn.Write([]byte("SignLanguage Request"))
+		if err != nil {
+			fmt.Println("Fail to write: ", err)
+			continue
+		}
+
+		// wait for word
+		word := <-wordChannel
+		// compare word
+		if word == clientJson.Word {
+			_, err = conn.Write([]byte("Correct"))
+			if err != nil {
+				fmt.Println("Fail to write: ", err)
+				continue
+			}
+		} else {
+			_, err = conn.Write([]byte("Incorrect"))
+			if err != nil {
+				fmt.Println("Fail to write: ", err)
+				continue
+			}
+		}
 	}
 }
 
@@ -113,6 +183,7 @@ func main() {
 	}(socket)
 
 	channel := make(chan []byte)
+	wordChannel := make(chan string)
 	for {
 		conn, err := socket.Accept()
 		if err != nil {
@@ -161,21 +232,16 @@ func main() {
 			fmt.Println("Connect: ", jsonFile.Api)
 			existingConn, ok := userMap[jsonFile.Api]
 			if ok {
-				write, err := existingConn.Write([]byte("1"))
-				if err != nil {
-					continue
-				}
-				fmt.Println("Write: ", write)
+				go webClientConnection(conn, existingConn, wordChannel)
 			} else {
-				write, err := conn.Write([]byte("Connection refused"))
+				_, err := conn.Write([]byte("Connection refused"))
 				if err != nil {
 					continue
 				}
-				fmt.Println("Write: ", write)
 			}
 		} else if jsonFile.Type == 3 {
 			fmt.Println("Model connected")
-			go modelServer(conn, channel)
+			go modelServer(conn, channel, wordChannel)
 		}
 	}
 }
