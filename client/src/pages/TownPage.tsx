339c8e8594f7034591009d306f6fcf55a5cead98
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2023-11-17 23:41:40 +0900

    feat: 게임 페이지로 이동하는 기능 추가

--- client/src/pages/TownPage.tsx
+++ client/src/pages/TownPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 import config from '@/game/config';
 import { emitter } from '@/game/scenes/constants';
@@ -10,12 +11,18 @@ import MultiVideo from '@/components/Video/MultiVideo';
 function TownPage() {
   const ref = useRef<HTMLDivElement>(null);
   const mySocket = useRecoilValue(socketState);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!mySocket.isReady) return;
 
     emitter.on('init', () => {
       emitter.emit('initPlayer', mySocket);
+    });
+
+    emitter.on('start quiz', () => {
+      console.log('start quiz');
+      navigate('/game');
     });
 
     if (!ref.current) return;
