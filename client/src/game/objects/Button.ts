76714b8376b956188c9882245932ecdfebf006ba
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2023-12-07 15:34:14 +0900

    feat: button game object 추가

--- client/src/game/objects/Button.ts
+++ client/src/game/objects/Button.ts
@@ -1,28 +1,50 @@
 import Phaser from 'phaser';
 
-interface Props {
-  scene: Phaser.Scene;
-  x: number;
-  y: number;
-  text: string;
-  fontSize?: number;
-  pointerDownFunc?: () => void;
-}
-
 export default class Button {
-  constructor({ scene, x, y, text, fontSize, pointerDownFunc }: Props) {
-    const button = scene.add
+  button: Phaser.GameObjects.Text;
+  scene: Phaser.Scene;
+
+  constructor(scene: Phaser.Scene, x: number, y: number, text: string, fontSize?: number, pointerDownFunc?: Function) {
+    this.scene = scene;
+    this.button = scene.add
       .text(x, y, text)
       .setOrigin(0.5)
       .setStyle({ font: 'MapleStory', fill: '#000000' })
-      .setFontSize(12);
+      .setFontSize(12)
+      .setFontFamily('MapleStory')
+      .setFontStyle('bold')
+      .setDepth(1);
 
-    if (fontSize) button.setFontSize(fontSize);
+    if (fontSize) this.button.setFontSize(fontSize);
     if (pointerDownFunc)
-      button
+      this.button
         .setInteractive()
         .on('pointerdown', pointerDownFunc, scene)
-        .on('pointerover', () => button.setStyle({ fill: '#f39c12' }))
-        .on('pointerout', () => button.setStyle({ fill: '#000000' }));
+        .on('pointerover', () => this.button.setStyle({ fill: '#f39c12' }))
+        .on('pointerout', () => this.button.setStyle({ fill: '#000000' }));
+  }
+
+  getBounds() {
+    return this.button.getBounds();
+  }
+
+  getX() {
+    return this.button.x;
+  }
+
+  setPosition(x: number, y: number) {
+    this.button.setPosition(x, y);
+  }
+
+  destroy() {
+    this.button.destroy();
+  }
+
+  setPointerDownFunction(pointerDownFunc: Function) {
+    this.button
+      .setInteractive()
+      .on('pointerdown', pointerDownFunc, this.scene)
+      .on('pointerover', () => this.button.setStyle({ fill: '#f39c12' }))
+      .on('pointerout', () => this.button.setStyle({ fill: '#000000' }));
   }
 }
